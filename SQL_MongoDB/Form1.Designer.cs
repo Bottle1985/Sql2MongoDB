6d0b517aee0b91b347381ab4d17572a4b68fa958
Bottle1985 <mrboongvn@yahoo.com.vn>
2015-11-04 07:16:42 +0700

    + Added 2 button

--- SQL_MongoDB/Form1.Designer.cs
+++ SQL_MongoDB/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -88,11 +90,33 @@
             this.textBox6.Size = new System.Drawing.Size(125, 20);
             this.textBox6.TabIndex = 6;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(772, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(772, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(988, 419);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -118,6 +142,8 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- SQL_MongoDB/Form1.cs
+++ SQL_MongoDB/Form1.cs
@@ -7,10 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
+// Insert Mongo
+using System.Threading.Tasks;
 // Mongo DB
 using MongoDB.Bson;
 using MongoDB.Driver;
+
 
 namespace SQL_MongoDB
 {
@@ -93,10 +95,67 @@ namespace SQL_MongoDB
         //_database = _client.GetDatabase("test");
         private void connect_MG()
         {
-         MongoServer mongo = MongoServer.Create();
-         mongo.Connect();
+            MongoServer mongo = MongoServer.Create();
+            //MongoServer mongo = MongoClient.GetServer();
+         
+            mongo.Connect();
 
-         var db = mongo.GetDatabase("test");
+            var db = mongo.GetDatabase("test");
+        }
+        private void insert_MongoDB()
+        {
+            MongoServer mongo = MongoServer.Create();
+      
+            mongo.Connect();
+
+            var db = mongo.GetDatabase("test");
+            var document = new BsonDocument
+            {
+                { "address" , new BsonDocument
+                    {
+                        { "street", "2 Avenue" },
+                        { "zipcode", "10075" },
+                        { "building", "1480" },
+                        { "coord", new BsonArray { 73.9557413, 40.7720266 } }
+                    }
+                },
+                { "borough", "Manhattan" },
+                { "cuisine", "Italian" },
+                { "grades", new BsonArray
+                    {
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "A" },
+                            { "score", 11 }
+                        },
+                        new BsonDocument
+                        {
+                            { "date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc) },
+                            { "grade", "B" },
+                            { "score", 17 }
+                        }
+                    }
+                },
+                { "name", "Vella" },
+                { "restaurant_id", "41704620" }
+            };
+
+            var collection = db.GetCollection<BsonDocument>("restaurants");
+            collection.Insert(document);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insert_MongoDB();
+        }
+        private void find_mongoDB()
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
